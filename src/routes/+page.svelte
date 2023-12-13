42f4e7b1fd238727cd41ed6031fc1e0a1792dd9c
milanrolie <112857190+milanrolie@users.noreply.github.com>
2023-12-13 12:58:05 +0100

    Old project added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,14 @@
 <script>
+<<<<<<< Updated upstream
   export let data;
   console.log(data)
 </script>
 
+=======
+    import Hero from "$lib/Hero.svelte";
+    import Navigation from "../lib/Navigation.svelte";
+  </script>
+  
+  <Navigation />
+  <Hero />
+>>>>>>> Stashed changes
