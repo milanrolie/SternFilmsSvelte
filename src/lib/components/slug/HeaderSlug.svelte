8f3010ebfce779b4cda850237406b08dac1d8a9c
milanrolie <112857190+milanrolie@users.noreply.github.com>
2023-12-21 13:30:05 +0100

    gekke overlay on mousemove

--- src/lib/components/slug/HeaderSlug.svelte
+++ src/lib/components/slug/HeaderSlug.svelte
@@ -8,98 +8,131 @@
   import Article from "./Article.svelte";
   import AllProjects from "../slug/Allprojects.svelte";
 
-
   import { onMount } from "svelte";
+  import { afterUpdate } from "svelte";
+
+  let showOverlay = false;
+  let timeout;
 
   onMount(() => {
-    // const video = document.querySelector("video");
+    window.addEventListener("mousemove", handleMouseMove);
+  });
 
-    // function showTimeline () {
-    // }
+  afterUpdate(() => {
+    if (showOverlay) {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        showOverlay = false;
+      }, 3000);
+    }
+  });
 
-  })
+  function handleMouseMove() {
+    showOverlay = true;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      showOverlay = false;
+    }, 3000);
+  }
 </script>
 
 <div class="video-container">
-  <video autoplay loop muted playsinline src={data.projectData.data.project.videoShortWebm.url}>
+  <div class="overlay" class:visible={showOverlay}></div>
+  <video
+    autoplay
+    loop
+    muted
+    playsinline
+    src={data.projectData.data.project.videoShortWebm.url}
+  >
   </video>
 </div>
-
-
 
 <section class="info-container">
   <InfoButton />
 
   <div class="buttonVideo">
-    <PauseButton  />
-    <MuteButton /> 
+    <PauseButton />
+    <MuteButton />
   </div>
 
   <h1>{data.projectData.data.project.title}</h1>
   <span></span>
 
   <div class="container-inner">
-    <Description data={data.projectData.data}/>
-    <Article data={data.projectData.data}/>
+    <Description data={data.projectData.data} />
+    <Article data={data.projectData.data} />
   </div>
 
-  <AllProjects data={data.allProjectsData.data}/>
+  <AllProjects data={data.allProjectsData.data} />
 </section>
 
-
-
 <style>
-.video-container {
-  position: sticky;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100vh;
-}
+  .video-container {
+    position: sticky;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100vh;
+  }
 
-video {
-  width: 100%;
-  height: 100vh;
-  object-fit: cover;
-}
+  .overlay {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: rgb(0,0,0);
+background: linear-gradient(180deg, rgba(0,0,0,0.5760679271708684) 0%, rgba(0,0,0,0) 100%);
+    opacity: 0;
+    transition: .3s ease-in-out;
+  }
 
-.info-container {
-  position: relative;
-  background-color: rgba(0, 0, 0, 0.635);
-  width: 100%;
-  left: 0;
-  height: 25vw;
-  color:var(--main-offwhite);
-  padding: var(--margin);
-  background-color: rgba(0, 0, 0, 0.675);
+  .overlay.visible {
+    opacity: .5;
+  }
+
+  video {
+    width: 100%;
+    height: 100vh;
+    object-fit: cover;
+  }
+
+  .info-container {
+    position: relative;
+    background-color: rgba(0, 0, 0, 0.635);
+    width: 100%;
+    left: 0;
+    height: 25vw;
+    color: var(--main-offwhite);
+    padding: var(--margin);
+    background-color: rgba(0, 0, 0, 0.675);
     backdrop-filter: blur(5px);
     -webkit-backdrop-filter: blur(5px);
-}
+  }
 
-span {
-  display: block;
-  width: 100%;
-  height: 1px;
-  background-color: white;
-  margin: 1rem 0;
-}
+  span {
+    display: block;
+    width: 100%;
+    height: 1px;
+    background-color: white;
+    margin: 1rem 0;
+  }
 
-h1 {
-  font-weight: 600;
-  font: 24px;
-}
+  h1 {
+    font-weight: 600;
+    font: 24px;
+  }
 
-.container-inner {
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-  height: 100%;
-}
+  .container-inner {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    height: 100%;
+  }
 
-.buttonVideo {
-  position: absolute;
-  gap: 0.5rem;
-  top: -3.5rem;
-  left: 1rem;
-}
+  .buttonVideo {
+    position: absolute;
+    gap: 0.5rem;
+    top: -3.5rem;
+    left: 1rem;
+  }
 </style>
